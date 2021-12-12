028b3430512a040715ed2579e4984e859b5cd926
mckotcia <mckotcia@syneforge.com>
2021-12-12 16:15:21 +0200

    add menu video

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,7 +2,7 @@
 using UnityEngine.Audio;
 using UnityEngine;
 
-public static class AudioManager: MonoBehaviour {
+public class AudioManager: MonoBehaviour {
    public static void PlaySound() {
     GameObject soundGameObject = new GameObject("Sound");
     AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
